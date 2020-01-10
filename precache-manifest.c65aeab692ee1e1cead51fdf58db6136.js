edb162041905cecd11bd56dfae95e729c73b3ce9
Peter Mazuryk <petermazuryk1984@gmail.com>
2020-01-10 22:02:55 +0200

    Updates

---
+++ precache-manifest.c65aeab692ee1e1cead51fdf58db6136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afdc8ddeb1fd4204d30acc2fd1a702c",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "8949c26369c3c7fad889",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "dd96f0c44d121c936048",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.259737c1.chunk.css"
+  },
+  {
+    "revision": "8949c26369c3c7fad889",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.aee7313e.chunk.js"
+  },
+  {
+    "revision": "dd96f0c44d121c936048",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.ba76cb47.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
